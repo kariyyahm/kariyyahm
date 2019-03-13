c69fc491c9e38f3fbb2304d9e9ee870a2651ac38
kari <mr_hm@outlook.com>
2019-03-13 17:42:44 +0800

    Site updated: 2019-03-13 17:42:44

--- js/gallery.js
+++ js/gallery.js
@@ -1,14 +1,16 @@
 (function($){
   // Caption
-  $('.entry-content').each(function(i){
+  $('.entry').each(function(i){
     $(this).find('img').each(function(){
-      var alt = this.alt;
+      if (!$(this).parent().is("a")) {
+        var alt = this.alt, title = this.title;
+        var caption = title ? title : alt;
+        if (caption){
+          $(this).after('<div class="caption"><i  class="fa fa-picture-o"></i>' + caption + '</div>');
+        }
 
-      if (alt){
-        $(this).after('<span class="caption">' + alt + '</span>');
+        $(this).wrap('<a href="' + this.src + '" title="' + caption + '" class="fancybox" rel="gallery' + i + '" />');
       }
-
-      $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox" rel="gallery' + i + '" />');
     });
   });
 
